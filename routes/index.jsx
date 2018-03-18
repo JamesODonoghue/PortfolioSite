57589c4e1bb5979990f9a5b26aff8b01420455de
James O'Donoghue <james.odonoghue@advent.com>
2018-03-18 12:54:17 -0700

    added support for server side routing (not sure if this is a hack but it works)

---
+++ routes/index.jsx
@@ -0,0 +1,11 @@
+var router = require('express').Router();
+var React = require('react');
+var ReactDOMServer = require('react-dom/server');
+var ReactRouter = require('react-router');
+var path = require('path');
+
+router.get('/*', function(request, response) {
+    response.sendFile(path.resolve('dist/index.html')); 
+});
+
+module.exports = router;
