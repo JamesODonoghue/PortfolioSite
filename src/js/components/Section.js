b972725584d9bf6893bb624b55f223e87f065df4
James O'Donoghue <jodonogh1@gmail.com>
2018-05-06 17:29:59 -0700

    added muni map project to list of projects

--- src/js/components/Section.js
+++ src/js/components/Section.js
@@ -26,19 +26,34 @@ export default class Section extends React.Component {
                     <div className="row">
                         <div className="col">
                             <div className="display-1">
-                                Here is a sample project 
+                                Here are some sample projects
                             </div>
                         </div>
                     </div>
                     <div className="row">
                         <div className="col">
-                            <button className="button-spotify" onClick={() => window.location.href='/spotify'}>
+                            <a href="/spotify">
+                                <button className="button-spotify">
                                     <div className="card-body">
                                         <h4 className="card-title"> Spotify Playlist Analyzer</h4>
                                         <p className="card-text"> Spotify Client that analyzes playlists. This app uses ReactJS and a Spotify API wrapper -- spotify-web-api </p>
-
                                     </div>
-                            </button>
+                                </button>
+                            </a>
+                            
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <a href='http://next-bus-map.herokuapp.com/'>
+                                <button className="button-muni-map">
+                                    <div className="card-body">
+                                        <h4 className="card-title"> SF Muni Map </h4>
+                                        <p className="card-text"> NextBus Client that uses D3 and the NextBusAPI to draw real time locations of SF Muni buses </p>
+                                    </div>
+                                </button>
+                            </a>
+                           
                         </div>
                     </div>
                 </div>
